babf5bc6f5e878fb6dd3820a15765c9b7c1491ef
dvdhoek <djpvanderhoek@gmail.com>
2016-05-23 16:21:18 +0800

    working version of videoapp, ready for implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,48 @@
 //= require turbolinks
 //= require_tree .
 
+/*
+to modify total time, just input on variable totaltime
+*/
+function startTimer () {
+var totaltime = 10;
+function update(percent){
+  var deg;
+  if(percent<(totaltime/2)){
+    deg = 90 + (360*percent/totaltime);
+      $('.pie').css('background-image',
+                'linear-gradient('+deg+'deg, transparent 50%, white 50%),linear-gradient(90deg, white 50%, transparent 50%)'
+               );
+  } else if(percent>=(totaltime/2)){
+          deg = -90 + (360*percent/totaltime);
+          $('.pie').css('background-image',
+                'linear-gradient('+deg+'deg, transparent 50%, #1fbba6 50%),linear-gradient(90deg, white 50%, transparent 50%)'
+               );
+          }
+}
+var less = 1
+var count = parseInt($('#time').text());
+myCounter = setInterval(function () {
+
+  count-=less;
+  $('#time').html(count);
+    update(count);
+
+  if(count==0) {
+    disconnect()
+    less = 0
+     // find proper fix for this issue. javascript won't stop :(
+  };
+  // if(count==0) clearInterval(myCounter);
+}, 1000);
+};
 
 // Initialize an OpenTok Session object
-var session = TB.initSession(sessionId);
+var session = TB.initSession(sessionId); 
 
 // Initialize a Publisher, and place it into the element with id="publisher"
-var publisher = TB.initPublisher(apiKey, 'publisher');
+var publisher = TB.initPublisher(apiKey, 'publisher', {width: 120, height: 90});
+
 
 // Attach event handlers
 session.on({
@@ -29,7 +65,7 @@ session.on({
   sessionConnected: function(event) {
     // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
     // clients)
-    session.publish(publisher);
+    session.publish(publisher, 'publisher', {width: 120, height: 90});
   },
 
   // This function runs when another client publishes a stream (eg. session.publish())
@@ -41,10 +77,23 @@ session.on({
     document.getElementById('subscribers').appendChild(subContainer);
 
     // Subscribe to the stream that caused this event, put it inside the container we just made
-    session.subscribe(event.stream, subContainer);
+    session.subscribe(event.stream, 'subscribers', {width: 480, height: 320}, startTimer);
   }
+
 
 });
 
 // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
 session.connect(apiKey, token);
+
+function disconnect() {
+  session.disconnect();
+}
+
+session.on("sessionDisconnected", function(event){
+  console.log("sessionDisconnected event fired");
+  // Session has been disconnected. Include any clean up code here
+});
+
+
+
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,9 +6,10 @@ require 'opentok'
 
 def index
 	@api_key = "45592332"
-	@opentok = OpenTok::OpenTok.new(@api_key, 'ce4771d7c91a3f2e773ae76b1d6d9f6db5301e5e')
-	@session = @opentok.create_session
+	# @opentok = OpenTok::OpenTok.new(@api_key, 'ce4771d7c91a3f2e773ae76b1d6d9f6db5301e5e')
+	# @session = @opentok.create_session
 
+	
 	@session_id = '1_MX40NTU5MjMzMn5-MTQ2MzgwMTY0NTgyM345cGp4cGdRWnFrUWR4dXRRYTl5a29lRUp-fg'
 	@token = 'T1==cGFydG5lcl9pZD00NTU5MjMzMiZzaWc9MjFlMmUyMzQxMDgzM2Y1MTk1OTU0MzYzYjNiNjE0MjQ1MmZhZGVlYTpzZXNzaW9uX2lkPTFfTVg0ME5UVTVNak16TW41LU1UUTJNemd3TVRZME5UZ3lNMzQ1Y0dwNGNHZFJXbkZyVVdSNGRYUlJZVGw1YTI5bFJVcC1mZyZjcmVhdGVfdGltZT0xNDYzODAxNjY3Jm5vbmNlPTAuMDM5MTI2Njc1ODA4ODAyMjUmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ2NDQwNjQ2NQ=='
 end
